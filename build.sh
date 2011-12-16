e45cdb96fb1083d1ee4b2320a03e5bae7cbffade
FloSoft <sf-team@siedler25.org>
2011-12-16 11:51:35 +0100

    dasdas

--- build.sh
+++ build.sh
@@ -3,12 +3,10 @@
 MOD_DIR=$(pwd)
 MCP_DIR=$MOD_DIR/..
 
-patch -Np1 mlprop.patch
-
-cp -a src/* $MCP_DIR/src/minecraft/net/minecraft/src
+cp -va src/* $MCP_DIR/src/minecraft/net/minecraft/src
 
 mkdir -p $MCP_DIR/bin/minecraft/net/minecraft/src/AdditionalBuildcraftObjects/gui
-cp -a src/AdditionalBuildcraftObjects/gui/*.png $MCP_DIR/bin/minecraft/net/minecraft/src/AdditionalBuildcraftObjects/gui
+cp -va src/AdditionalBuildcraftObjects/gui/*.png $MCP_DIR/bin/minecraft/net/minecraft/src/AdditionalBuildcraftObjects/gui
 
 pushd $MCP_DIR
 	./recompile.sh || exit 1
