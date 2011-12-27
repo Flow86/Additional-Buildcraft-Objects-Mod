dfd5c066e402c42593ef07dcba0466e958bceef9
FloSoft <sf-team@siedler25.org>
2011-12-27 15:48:39 +0100

    trigger update

---
+++ src/AdditionalBuildcraftObjects/ABOTriggerProvider.java
@@ -0,0 +1,47 @@
+/** 
+ * Copyright (C) 2011 Flow86
+ * 
+ * AdditionalBuildcraftObjects is open-source.
+ *
+ * It is distributed under the terms of my Open Source License. 
+ * It grants rights to read, modify, compile or run the code. 
+ * It does *NOT* grant the right to redistribute this software or its 
+ * modifications in any form, binary or source, except if expressively
+ * granted by the copyright holder.
+ */
+
+package net.minecraft.src.AdditionalBuildcraftObjects;
+
+import java.util.LinkedList;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.TileEntity;
+import net.minecraft.src.mod_AdditionalBuildcraftObjects;
+import net.minecraft.src.buildcraft.api.IPipe;
+import net.minecraft.src.buildcraft.api.ITriggerProvider;
+import net.minecraft.src.buildcraft.api.Trigger;
+import net.minecraft.src.buildcraft.energy.TileEngine;
+
+/**
+ * @author Flow86
+ * 
+ */
+public class ABOTriggerProvider implements ITriggerProvider {
+
+	@Override
+	public LinkedList<Trigger> getPipeTriggers(IPipe pipe) {
+		return new LinkedList<Trigger>();
+	}
+
+	@Override
+	public LinkedList<Trigger> getNeighborTriggers(Block block, TileEntity tile) {
+		LinkedList<Trigger> result = new LinkedList<Trigger>();
+
+		if (tile instanceof TileEngine) {
+			result.add(mod_AdditionalBuildcraftObjects.triggerEngineControl);
+		}
+
+		return result;
+	}
+
+}
